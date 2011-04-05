4a3c0c6911d588cba30077e582001d31e04c9748
leandronsp <leandronsp@gmail.com>
2011-04-05 14:12:29 -0300

    implementation of arrows at slides; changes on script js

--- slide.js
+++ slide.js
@@ -1,50 +1,55 @@
-jQuery.fn.slide = function(width) {
-  return this.each(function(){
+
 		
-		var position = 0;
-		var slideWidth = width;
+	  	var currentPosition = 0;
+		var slideWidth = $('.slide').width();
 		var slides = $('.slide');
-		pages = slides.length;		
+		var numberOfSlides = slides.length;		
 		
 		$('#slidesContainer').css('overflow', 'hidden');
 
-		slides
-			.wrapAll('<div id="slideInner"></div>')
-			.css({
+		slides.wrapAll('<div id="slideInner"></div>').css({
 			'float' : 'left',
 			'width' : slideWidth
 		});
 
 
-		$('#slideInner').css('width', slideWidth * pages);
-
+		$('#slideInner').css('width', slideWidth * numberOfSlides);
+		
 		$('#slideshow')
-			.prepend('<span class="control" id="leftControl">left</span>')
-			.append('<span class="control" id="rightControl">right</span>');
-
-		manageControls(position);
-
-		$('.control')
-			.bind('click', function(){
-
-			position = ($(this).attr('id')=='rightControl') ? position+1 : position-1;
-
-			manageControls(position);
-			$('#slideInner').animate({
-				'marginLeft' : slideWidth*(-position)
-			});
-
+		.prepend('<span class="control" id="leftControl">Move left</span>')
+		.append('<span class="control" id="rightControl">Move right</span>');
+		
+		$("#slideshow").mouseenter(function() {
+				$("#rightControl").show();
+				$("#leftControl").show();
+			}).mouseleave(function() {
+				$("#rightControl").hide();
+				$("#leftControl").hide();
 		});
-			
-  });
-};
 
-// manageControls: Hides and Shows controls depending on position
- function manageControls(position){
-   // Hide left arrow if position is first slide
-	if(position==0){ $('#leftControl').hide() } else{ $('#leftControl').show() }
-	// Hide right arrow if position is last slide
-   if(position==pages-1){ $('#rightControl').hide() } else{ $('#rightControl').show() }
- }
+
+		$('.control').click(function(){
+			if ($(this).attr('id')=='leftControl') {
+				if (currentPosition == 0) {
+				  currentPosition = numberOfSlides-1;
+				} else {
+				  currentPosition = currentPosition - 1;
+				}
+			} else {
+				if (currentPosition == numberOfSlides-1) {
+				  currentPosition = 0;
+				} else {
+				  currentPosition = currentPosition + 1;
+				}
+
+			}
+
+			$('#slideInner').stop().animate({
+			'marginLeft' : slideWidth*(-currentPosition)
+			});
+		});
+		
+			
+
 
 
